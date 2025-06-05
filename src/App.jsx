ceb0d9f3d717b0194188eef7829d4029cf40f459
Ankit-Hub-Max <ankitsingh623@gmail.com>
2025-06-05 10:47:45 +0530

    fix the alert and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         setUsers(data)
       }catch(error){
         console.error("Failed to fetch users:",error);
+        alert("Failed to fetch users")
       }
     }
     fetchUsers();
